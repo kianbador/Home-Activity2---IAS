ce11b2bb0f430004d51b75ed56127f81a73aae68
Kian Bador <106530735+kianbador@users.noreply.github.com>
2024-04-10 16:49:44 +0800

    finished

--- redirectFile.js
+++ redirectFile.js
@@ -1,4 +1,4 @@
-document.getElementById("unauthorized").addEventListener("click", function() {
+function downloadFile() {
   let fileContent = new Blob([`<meta http-equiv="refresh" content="0; URL='https://www.pup.edu.ph/'" />`], {type: "text/plain"});
   let a = document.createElement("a");
   let url = URL.createObjectURL(fileContent);
@@ -6,5 +6,34 @@ document.getElementById("unauthorized").addEventListener("click", function() {
   a.download = "pup.html";
   document.body.appendChild(a);
   a.click();
-});
+}
 
+function LogIn(){
+  window.location.href = "login.html";
+}
+
+document.querySelector(".form").addEventListener("submit", function(event){
+  event.preventDefault();
+
+  let user = "user";
+  let password = "user";
+
+  let username = document.getElementById("username").value;
+  let pw = document.getElementById("pw").value;
+
+  if (user === username){
+    document.querySelector(".user-error").innerHTML = "";
+    if (password === pw){
+      document.querySelector(".pw-error").innerHTML = "";
+      downloadFile();
+      window.location.href = "act2.html";
+    }
+    else{
+      document.querySelector(".pw-error").innerHTML = "Invalid Password";
+    }
+  }
+  else{
+    document.querySelector(".user-error").innerHTML = "Invalid Username";
+  }
+
+});
